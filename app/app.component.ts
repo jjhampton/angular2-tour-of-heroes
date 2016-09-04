92df4271dbd9c6c5892324a54560ba18c6be6bbf
John Hampton <john_j_hampton@yahoo.com>
2016-09-03 20:50:25 -0400

    Change heroService to return immediately-resolved Promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -81,7 +81,7 @@ export class AppComponent implements OnInit {
     selectedHero: Hero;
     heroes: Hero[];
     getHeroes(): void {
-        this.heroes = this.heroService.getHeroes();
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
