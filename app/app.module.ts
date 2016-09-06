70fd669d16fa93046ee084303ef1d6060cae315e
John Hampton <john_j_hampton@yahoo.com>
2016-09-05 21:53:13 -0400

    Add first draft at creating HeroesComponent and refactoring out code in AppComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
   imports: [
@@ -12,7 +17,11 @@ import { AppComponent }  from './app.component';
   ],
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
